32f884241b4f749dcec7415d2b9f24a1b3eb4bd6
GrowlyX <growlyxdev@gmail.com>
2023-04-21 00:15:06 -0400

    feat: start work on plugin

---
+++ plugin/src/main/kotlin/gg/tropic/uhc/plugin/TropicUHCPlugin.kt
@@ -0,0 +1,23 @@
+package gg.tropic.uhc.plugin
+
+import gg.scala.commons.ExtendedScalaPlugin
+import gg.scala.commons.core.plugin.Plugin
+import gg.scala.commons.core.plugin.PluginAuthor
+import gg.scala.commons.core.plugin.PluginDependency
+import gg.scala.commons.core.plugin.PluginDependencyComposite
+
+/**
+ * @author GrowlyX
+ * @since 4/21/2023
+ */
+@Plugin(
+    name = "UHC",
+    version = "%remote%/%branch%/%id%",
+    description = "The plugin for Tropic's UHC gamemode."
+)
+@PluginAuthor("GrowlyX")
+@PluginDependencyComposite(
+    PluginDependency("scala-commons"),
+    PluginDependency("ScGameFramework")
+)
+class TropicUHCPlugin : ExtendedScalaPlugin()
