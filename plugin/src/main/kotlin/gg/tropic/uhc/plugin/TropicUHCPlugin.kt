807c4c68731ffa25590790b07dc748ccaab7728e
GrowlyX <growlyxdev@gmail.com>
2023-04-22 15:52:52 -0400

    fix: don't set arena world to lobby

--- plugin/src/main/kotlin/gg/tropic/uhc/plugin/TropicUHCPlugin.kt
+++ plugin/src/main/kotlin/gg/tropic/uhc/plugin/TropicUHCPlugin.kt
@@ -1,6 +1,5 @@
 package gg.tropic.uhc.plugin
 
-import gg.scala.cgs.common.information.arena.CgsGameArenaHandler
 import gg.scala.commons.ExtendedScalaPlugin
 import gg.scala.commons.annotations.container.ContainerEnable
 import gg.scala.commons.core.plugin.Plugin
@@ -8,7 +7,6 @@ import gg.scala.commons.core.plugin.PluginAuthor
 import gg.scala.commons.core.plugin.PluginDependency
 import gg.scala.commons.core.plugin.PluginDependencyComposite
 import gg.tropic.uhc.plugin.engine.UHCGameEngine
-import org.bukkit.Bukkit
 
 /**
  * @author GrowlyX
@@ -36,9 +34,5 @@ class TropicUHCPlugin : ExtendedScalaPlugin()
         flavor {
             bind<UHCGameEngine>() to engine
         }
-
-        // TODO: change to proper world after world gen
-        CgsGameArenaHandler.world = Bukkit
-            .getWorld("lobby")
     }
 }
