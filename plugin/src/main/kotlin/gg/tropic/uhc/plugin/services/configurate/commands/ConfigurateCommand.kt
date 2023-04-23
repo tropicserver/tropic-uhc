920e236659a0daf8276880d8bc1433220f93ea20
GrowlyX <growlyxdev@gmail.com>
2023-04-23 12:41:32 -0400

    feat: add printconfig command

--- plugin/src/main/kotlin/gg/tropic/uhc/plugin/services/configurate/commands/ConfigurateCommand.kt
+++ plugin/src/main/kotlin/gg/tropic/uhc/plugin/services/configurate/commands/ConfigurateCommand.kt
@@ -4,7 +4,9 @@ import gg.scala.commons.acf.annotation.CommandAlias
 import gg.scala.commons.annotations.commands.AutoRegister
 import gg.scala.commons.command.ScalaCommand
 import gg.scala.commons.issuer.ScalaPlayer
+import gg.tropic.uhc.plugin.services.configurate.configurables
 import gg.tropic.uhc.plugin.services.configurate.menu.ConfigurateMenu
+import net.evilblock.cubed.util.CC
 
 /**
  * @author GrowlyX
@@ -17,5 +19,17 @@ object ConfigurateCommand : ScalaCommand()
     fun onConfig(player: ScalaPlayer)
     {
         ConfigurateMenu().openMenu(player.bukkit())
+    }
+
+    @CommandAlias("printconfig")
+    fun onPrintConfig(player: ScalaPlayer)
+    {
+        player.sendMessage("${CC.GREEN}Game Config:")
+
+        configurables.forEach {
+            player.sendMessage(
+                "${CC.GRAY}${it.name}: ${CC.GREEN}${it.value}"
+            )
+        }
     }
 }
