66a5fa0f991f2181a81f7b68a435b6222f0a7547
GrowlyX <growlyxdev@gmail.com>
2023-04-23 20:08:58 -0400

    feat: add /uhc game-stats

--- plugin/src/main/kotlin/gg/tropic/uhc/plugin/engine/commands/UHCCommand.kt
+++ plugin/src/main/kotlin/gg/tropic/uhc/plugin/engine/commands/UHCCommand.kt
@@ -8,12 +8,14 @@ import gg.scala.commons.acf.annotation.Default
 import gg.scala.commons.acf.annotation.Description
 import gg.scala.commons.acf.annotation.HelpCommand
 import gg.scala.commons.acf.annotation.Subcommand
+import gg.scala.commons.acf.bukkit.contexts.OnlinePlayer
 import gg.scala.commons.annotations.commands.AssignPermission
 import gg.scala.commons.annotations.commands.AutoRegister
 import gg.scala.commons.command.ScalaCommand
 import gg.scala.commons.issuer.ScalaPlayer
 import gg.scala.flavor.inject.Inject
 import gg.tropic.uhc.plugin.TropicUHCPlugin
+import gg.tropic.uhc.plugin.services.scenario.profile
 import gg.tropic.uhc.plugin.services.styles.prefix
 import net.evilblock.cubed.util.CC
 import org.bukkit.metadata.FixedMetadataValue
@@ -55,6 +57,21 @@ object UHCCommand : ScalaCommand()
 
         player.sendMessage(
             "$prefix${CC.GREEN}You've enabled X-Ray alerts."
+        )
+    }
+
+    @Subcommand("game-stats")
+    @Description("View current game stats for a player.")
+    fun onGameStats(player: ScalaPlayer, target: OnlinePlayer)
+    {
+        val model = target.player.profile
+        player.sendMessage(
+            "${CC.GREEN}${target.player.name}'s statistics:",
+            "${CC.GRAY}Coal Mined: ${CC.GREEN}${model.coalMined.value}",
+            "${CC.GRAY}Lapis Mined: ${CC.GREEN}${model.lapisMined.value}",
+            "${CC.GRAY}Gold Mined: ${CC.GREEN}${model.goldMined.value}",
+            "${CC.GRAY}Diamond Mined: ${CC.GREEN}${model.diamondsMined.value}",
+            "${CC.GRAY}Iron Mined: ${CC.GREEN}${model.ironMined.value}",
         )
     }
 }
